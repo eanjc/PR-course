13bc40d9a145ce38792ef49b9ce54e9a44f7b0c3
eanjc <xqsx45zy@126.com>
2019-04-25 16:58:40 +0800

    first upload

---
+++ Bayes/code/bayesModel.py
@@ -0,0 +1,91 @@
+import numpy as np
+from code import imgProcess
+import math
+class BayesModel:
+    def __init__(self,train_image,train_label,dim):
+        self.train_image=train_image
+        self.train_label=train_label
+        self.dim=dim
+        self.hasModel=False
+        self.PW=np.empty(10,dtype=np.float)
+        self.PJW=np.empty(shape=(10, self.dim), dtype=np.float)
+
+    def train_model(self):
+        NI={0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0}
+        if len(self.train_image)!=len(self.train_label):
+            print("Number of train_image is not equal to number of train_label !")
+            return
+        N=len(self.train_label)
+        for lable in self.train_label:
+            NI[int(lable)]+=1
+        PNI={}
+        for key in NI:
+            PNI[key]=float(NI[key]/N)
+
+        NJW=np.zeros(shape=(10,self.dim),dtype=np.int)
+        PJW = np.zeros(shape=(10, self.dim), dtype=np.float)
+
+
+        for i in range(N):
+            cur_label=int(self.train_label[i])
+            cur_raw_img=self.train_image[i]
+            img_inLine=np.ndarray.flatten(cur_raw_img,order='C')
+            for order in range(len(img_inLine)):
+                NJW[cur_label][order]+=img_inLine[order]
+            if i%500==0:
+                print("Has trained %d pictures."%i)
+
+        for i in range(10):
+            for j in range(self.dim):
+                PJW[i][j]=float((NJW[i][j]+1)/(NI[i]+2))
+
+        # PXW=np.zeros(10,dtype=np.float)
+        # for i in range(10):
+        #     res=float(1)
+        #     for j in range(self.dim):
+        #         res=res*PJW[i][j]
+        #     PXW[i]=res
+
+        PW=np.zeros(10,dtype=np.float)
+        for i in range(10):
+            PW[i]=float(NI[i]/N)
+
+        self.hasModel=True
+        self.PW=PW
+        self.PJW=PJW
+        print("Training model finished .")
+
+        return (PW,PJW)
+
+    def predict(self,test_image,test_label):
+        if self.hasModel==False:
+            print("Model has not trained!")
+            return
+
+        N=len(test_image)
+        correct=0
+        for i in range(N):
+            p={0:0.0,1:0.0,2:0.0,3:0.0,4:0.0,5:0.0,6:0.0,7:0.0,8:0.0,9:0.0}
+            img=np.ndarray.flatten(test_image[i])
+            for type in range(10):
+                res=math.log(self.PW[type])
+                for k in range(self.dim):
+                    if img[k]==1:
+                        res = res +math.log( self.PJW[type][k])
+                    else:
+                        res = res+math.log  (1-self.PJW[type][k])
+                p[type]=res
+            sortted_p=sorted(p.items(), key=lambda x: x[1], reverse=True)
+            print("Picture_Num : %d , predict result is %d , real result is %d ."%(i,int(sortted_p[0][0]),int(test_label[i])))
+            if int(sortted_p[0][0])==int(test_label[i]):
+                correct+=1
+
+        acc=correct/N
+        return acc
+
+
+
+
+
+
+
---
+++ Bayes/code/demo.py
@@ -0,0 +1,41 @@
+from code import loadData
+from code import imgProcess
+from code import  bayesModel
+import numpy as np
+
+# 训练集文件
+train_images_idx3_ubyte_file = '../data/train-images-idx3-ubyte/train-images.idx3-ubyte'
+# 训练集标签文件
+train_labels_idx1_ubyte_file = '../data/train-labels-idx1-ubyte/train-labels.idx1-ubyte'
+
+# 测试集文件
+test_images_idx3_ubyte_file = '../data/t10k-images-idx3-ubyte/t10k-images.idx3-ubyte'
+# 测试集标签文件
+test_labels_idx1_ubyte_file = '../data/t10k-labels-idx1-ubyte/t10k-labels.idx1-ubyte'
+
+train_images = loadData.load_train_images()
+train_labels = loadData.load_train_labels()
+
+test_images= loadData.load_test_images()
+test_labels=loadData.load_test_labels()
+
+train_images_step1=np.empty((len(train_images), 28, 28))
+test_images_step1=np.empty((len(test_images), 28, 28))
+
+
+for i in range(len(train_images)):
+    img_step1=imgProcess.ImgProcess().gray2bw_standard(train_images[i])
+    train_images_step1[i]=img_step1
+    if(i%500==0):
+        print("Has translated %d training pictures."%i)
+
+for i in range(len(test_images)):
+    img_step1=imgProcess.ImgProcess().gray2bw_standard(test_images[i])
+    test_images_step1[i]=img_step1
+    if(i%500==0):
+        print("Has translated %d test pictures."%i)
+
+bayesDemo=bayesModel.BayesModel(train_images_step1,train_labels,28*28)
+bayesDemo.train_model()
+ACC=bayesDemo.predict(test_images_step1,test_labels)
+print("Predict acc is %f ."%ACC)
